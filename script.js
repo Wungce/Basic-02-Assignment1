f63750e08c0c2e643ef0a59eaa8d757eb325d99e
wangsh <wugcelama19@gmail.com>
2021-06-13 15:49:04 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+//fing out that variable length.
+let MyName = "Wangsh";
+
+//variable call.
+console.log(MyName.length);
+
+
+//substr.
+let firstProgram = "Hello World!";
+
+let firstProgra = firstProgram.substr(6, 6);
+
+console.log(firstProgra);   
+
+
+
+
+
+
+
+
+
